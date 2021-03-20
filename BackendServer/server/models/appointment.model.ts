1e2ab977a7489bd204f7a8191807fb1b1c89fdd5
nguyenthaitan <nguyenthaitan9@gmail.com>
2021-03-21 06:26:27 +0700

    create model appointment

---
+++ BackendServer/server/models/appointment.model.ts
@@ -0,0 +1,27 @@
+import * as mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+const AppointmentSchema = new Schema(
+  {
+    doctor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "doctors",
+      required: true,
+    },
+    patient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "patients",
+      required: true,
+    },
+    time: { type: Number, required: true },
+    is_expired: {
+      type: Boolean,
+      required: true,
+    },
+    created_at: { type: Number },
+    updated_at: { type: Number },
+  },
+  { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
+);
+
+export const appointment = mongoose.model("appointments", AppointmentSchema);
