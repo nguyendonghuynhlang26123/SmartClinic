b17134ebd7faf33fcf3a046643dc4a4c2874a367
nguyenthaitan <nguyenthaitan9@gmail.com>
2021-03-21 08:55:30 +0700

    api get one + all doctor

---
+++ BackendServer/server/modules/doctors/doctor.controller.ts
@@ -0,0 +1,15 @@
+import * as express from "express";
+import { DoctorService } from "./doctor.service";
+const router = express.Router();
+
+const doctorService: DoctorService = new DoctorService();
+
+router.get("/", async (req, res) => {
+  const doctors = await doctorService.getAllDoctor();
+  res.json(doctors);
+});
+
+router.get("/:doctor_id", async (req, res) => {
+  const doctor = await doctorService.getDoctorById(req.params.doctorId);
+  res.json(doctor);
+});
