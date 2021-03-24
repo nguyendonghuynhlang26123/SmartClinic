2e1312e8a5fdd17c057943ecd08e14c339c19e3b
nguyendonghuynhlang26123 <nguyendonghuynhlang@gmail.com>
2021-03-24 20:02:35 +0700

    Patients register/login

--- BackendServer/server/modules/auth/auth.api.ts
+++ BackendServer/server/modules/auth/auth.api.ts
@@ -1,10 +1,18 @@
 import * as express from 'express';
+import { UserService } from './../users/user.service';
 import { AuthService } from './auth.service';
+import { PatientService } from '../patients/patient.service';
 const router = express.Router();
 
 const authService: AuthService = new AuthService();
 
-router.post('/login', (req, res) => {
+router.get('/logout', (req, res) => {
+  req.session.destroy(() => {
+    //res.redirect('/login');
+  });
+});
+
+router.post('/patients/login', (req, res) => {
   authService.authenticate(req.body.phone, req.body.password, (err, user) => {
     if (user) {
       req.session.regenerate(() => {
@@ -15,18 +23,25 @@ router.post('/login', (req, res) => {
       });
     } else {
       //req.session.error = `${err}. Authentication failed, please check your username and password.`;
-      res.json({
-        status: 'unsuccessful',
-        err: `${err}. Authentication failed, please check your username and password.`,
-      });
+      res.status(400).json({ 'message': 'Invalid credentials' });
     }
   });
 });
 
-router.get('/logout', (req, res) => {
-  req.session.destroy(() => {
-    res.redirect('/login');
-  });
+router.post('/patients/register', async (req, res) => {
+  const userSevice = new UserService();
+  const patientService = new PatientService();
+  try {
+    let newPatient = await patientService.createPatient(req.body);
+    let user = await userSevice.createUser({
+      ...req.body,
+      user_type: 'PATIENT',
+      user_infor: newPatient._id,
+    });
+    res.json(user);
+  } catch (err) {
+    res.status(400).json({ 'message': err.message });
+  }
 });
 
 export const AuthController = router;
