ececd26b579f54803e96de0537424009c94ff974
nguyendonghuynhlang26123 <nguyendonghuynhlang@gmail.com>
2021-03-24 19:49:20 +0700

    Add user create

--- BackendServer/server/modules/patients/patient.controller.ts
+++ BackendServer/server/modules/patients/patient.controller.ts
@@ -1,17 +1,27 @@
-import * as express from "express";
-import { PatientService } from "./patient.service";
+import * as express from 'express';
+import { PatientService } from './patient.service';
 const router = express.Router();
 
 const patientService: PatientService = new PatientService();
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   const patients = await patientService.getAllPatient();
   res.json(patients);
 });
 
-router.get("/:patient_id", async (req, res) => {
+router.get('/:patient_id', async (req, res) => {
   const patient = await patientService.getPatientById(req.params.patient_id);
   res.json(patient);
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const patient = await patientService.createPatient(req.body);
+    res.json(patient);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ 'message': err.message });
+  }
 });
 
 export const PatientController = router;
