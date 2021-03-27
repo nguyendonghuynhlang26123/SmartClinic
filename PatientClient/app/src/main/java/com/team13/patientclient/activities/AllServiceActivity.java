76db15b31c037aa7edd8876280f35ed1b6b47bbe
18127003 <seadog150600@gmail.com>
2021-03-27 16:37:19 +0700

    All service screen

--- PatientClient/app/src/main/java/com/team13/patientclient/activities/AllServiceActivity.java
+++ PatientClient/app/src/main/java/com/team13/patientclient/activities/AllServiceActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.team13.patientclient.R;
 
-public class DepartmentActivity extends AppCompatActivity {
+public class AllServiceActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_department);
+        setContentView(R.layout.activity_all_service);
     }
 }
