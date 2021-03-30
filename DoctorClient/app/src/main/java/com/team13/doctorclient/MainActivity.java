ce4967cf56f33f3486818f39f77b124498fa6ed7
18127003 <74701565+18127016@users.noreply.github.com>
2021-03-30 11:39:08 +0700

    Main activity

--- DoctorClient/app/src/main/java/com/team13/doctorclient/MainActivity.java
+++ DoctorClient/app/src/main/java/com/team13/doctorclient/MainActivity.java
@@ -1,14 +1,56 @@
 package com.team13.doctorclient;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity  {
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFragment(new HomeFragment());
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.request);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Fragment fragment;
+            switch (item.getItemId()){
+                case R.id.request:
+                    fragment = new HomeFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.schedule:
+                    fragment = new ScheduleFragment();
+                    loadFragment(fragment);
+                    return true;
+                default:
+                    return false;
+            }
+        });
+        BadgeDrawable badge = bottomNavigationView.getOrCreateBadge(R.id.mess);
+        badge.setVisible(true);
+        badge.setNumber(99);
     }
+
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+
+
 }
