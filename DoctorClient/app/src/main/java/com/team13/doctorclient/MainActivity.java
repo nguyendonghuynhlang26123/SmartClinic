2f03663585ed138f23f9f897bb3f04266561631b
18127016 <seadog150600@gmail.com>
2021-04-01 11:21:59 +0700

    Fix main and add resource

--- DoctorClient/app/src/main/java/com/team13/doctorclient/MainActivity.java
+++ DoctorClient/app/src/main/java/com/team13/doctorclient/MainActivity.java
@@ -10,5 +10,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+<<<<<<< Updated upstream
+=======
+        loadFragment(new ScheduleFragment());
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.request);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Fragment fragment;
+            switch (item.getItemId()){
+                case R.id.blog:
+                    fragment = new BlogFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.schedule:
+                    fragment = new ScheduleFragment();
+                    loadFragment(fragment);
+                    return true;
+                default:
+                    return false;
+            }
+        });
+        BadgeDrawable badge = bottomNavigationView.getOrCreateBadge(R.id.blog);
+        badge.setVisible(true);
+        badge.setNumber(99);
+>>>>>>> Stashed changes
     }
 }
